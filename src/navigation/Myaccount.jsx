6cec531c401fc00767007ac0e6149f192fa9507f
contrerasjake <contreras.jaspherjake@gmail.com>
2021-11-04 14:22:57 +0800

    Added login and rerouting

--- src/navigation/Myaccount.jsx
+++ src/navigation/Myaccount.jsx
@@ -8,6 +8,9 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
+import AuthService from "../services/auth.service";
+import { useHistory } from "react-router-dom";
+
 const useStyles = makeStyles((theme) => ({
     root: {
       height: '90vh',
@@ -115,227 +118,234 @@ function MyAccount() {
 
 	//Style
 	const classes = useStyles();
+	let history = useHistory();
+	const user = AuthService.getCurrentUser()
 
-	return (
-		<Grid container component="main" className={classes.root}>
-			
-		<CssBaseline/>
-			<Grid item xs={12} sm={12} md={12} component={Paper} elevation={2} square className={classes.myaccountContainer}>
-			
+	if (user) {
+		return (
+			<Grid container component="main" className={classes.root}>
 				
-					<div>
-						<Typography  component="h2" variant="h4" className={classes.h1}>MY ACCOUNT</Typography>
-					</div>
+			<CssBaseline/>
+				<Grid item xs={12} sm={12} md={12} component={Paper} elevation={2} square className={classes.myaccountContainer}>
 				
-
-				<div className={classes.paper}>
-					<form className={classes.form} noValidate>
-						<Grid container spacing={2}>
-							<Grid item xs={12} sm={4}>
-								<div className={classes.divHeader}>
-								<Typography className={classes.header}>
-									USER INFORMATION
-								</Typography>
-								{/* <Button 
-								type="submit"
-								fullWidth
-								variant="contained"
-								className={classes.button}
-								href="#"
-								>
-								UPLOAD
-								</Button> */}
-								</div>
-							</Grid>
-							<Grid item xs={12} sm={4}>
-								<div className={classes.divHeader}>
-								<Typography className={classes.header}>
-									PASSWORD
-								</Typography>
-								{/* <Button 
-								type="submit"
-								fullWidth
-								variant="contained"
-								className={classes.button}
-								href="#"
-								>
-								UPLOAD
-								</Button> */}
-								</div>
-							</Grid>
-							<Grid item xs={12} sm={4}></Grid>
-							
-							<Grid item xs={12} sm={3}>
-								{/* First Name */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
+					
+						<div>
+							<Typography  component="h2" variant="h4" className={classes.h1}>MY ACCOUNT</Typography>
+						</div>
+					
+	
+					<div className={classes.paper}>
+						<form className={classes.form} noValidate>
+							<Grid container spacing={2}>
+								<Grid item xs={12} sm={4}>
+									<div className={classes.divHeader}>
+									<Typography className={classes.header}>
+										USER INFORMATION
+									</Typography>
+									{/* <Button 
+									type="submit"
 									fullWidth
-									id="first_name"
-									label="First name"
-									name="first_name"
-									autoComplete="first_name"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* Last Name */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="last_name"
-									label="Last Name"
-									name="last_name"
-									autoComplete="last_name"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* Middle Name */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="middle_name"
-									label="Middle Name"
-									name="Middle Name"
-									autoComplete="middle_name"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* E-mail */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="E-mail"
-									name="email"
-									autoComplete="email"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* Contact Number */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="contact_number"
-									label="Contact Number"
-									name="contact_number"
-									autoComplete="contact_number"
-									autoFocus
-									/>
-								</Grid>
-							</Grid>
-							<Grid item xs={12} sm={1}></Grid>
-							<Grid item xs={12} sm={3}>
-								{/* Old Password */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="old_password"
-									label="Old Password"
-									name="old_password"
-									autoComplete="old_password"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* New Password */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="new_password"
-									label="New Password"
-									name="new_password"
-									autoComplete="new_password"
-									autoFocus
-									/>
-								</Grid>
-
-								{/* Confirm New Password */}
-								<Grid item xs={12} sm={12}>
-									<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									id="confirm_new_password"
-									label="Confirm New Password"
-									name="confirm_new_password"
-									autoComplete="confirm_new_password"
-									autoFocus
-									/>
-								</Grid>
-							</Grid>	
-								
-							<Grid item xs={12} sm={1}></Grid>
-							<Grid item xs={12} sm={3}>
-								<Grid item xs={12} sm={12} className={classes.uploadContainer}>
-									Upload your selfie 
-									<div className={classes.divtest}>
-										<img src="https://image.flaticon.com/icons/png/512/149/149092.png" width = "170px" height = "150px"></img>
-										<Grid item xs={12} sm={2}>
-											<div className={classes.picture}>
-												<Button 
-												type="submit"
-												fullWidth
-												variant="contained"
-												className={classes.buttonUpload}
-												href="#"
-												>
-												UPLOAD
-												</Button>
-												<Button 
-												type="submit"
-												fullWidth
-												variant="contained"
-												className={classes.buttonUpload}
-												href="#"
-												>
-												DELETE
-												</Button>
-											</div>
-										</Grid>    
+									variant="contained"
+									className={classes.button}
+									href="#"
+									>
+									UPLOAD
+									</Button> */}
 									</div>
 								</Grid>
+								<Grid item xs={12} sm={4}>
+									<div className={classes.divHeader}>
+									<Typography className={classes.header}>
+										PASSWORD
+									</Typography>
+									{/* <Button 
+									type="submit"
+									fullWidth
+									variant="contained"
+									className={classes.button}
+									href="#"
+									>
+									UPLOAD
+									</Button> */}
+									</div>
+								</Grid>
+								<Grid item xs={12} sm={4}></Grid>
+								
+								<Grid item xs={12} sm={3}>
+									{/* First Name */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="first_name"
+										label="First name"
+										name="first_name"
+										autoComplete="first_name"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* Last Name */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="last_name"
+										label="Last Name"
+										name="last_name"
+										autoComplete="last_name"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* Middle Name */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="middle_name"
+										label="Middle Name"
+										name="Middle Name"
+										autoComplete="middle_name"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* E-mail */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="email"
+										label="E-mail"
+										name="email"
+										autoComplete="email"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* Contact Number */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="contact_number"
+										label="Contact Number"
+										name="contact_number"
+										autoComplete="contact_number"
+										autoFocus
+										/>
+									</Grid>
+								</Grid>
+								<Grid item xs={12} sm={1}></Grid>
+								<Grid item xs={12} sm={3}>
+									{/* Old Password */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="old_password"
+										label="Old Password"
+										name="old_password"
+										autoComplete="old_password"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* New Password */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="new_password"
+										label="New Password"
+										name="new_password"
+										autoComplete="new_password"
+										autoFocus
+										/>
+									</Grid>
+	
+									{/* Confirm New Password */}
+									<Grid item xs={12} sm={12}>
+										<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										id="confirm_new_password"
+										label="Confirm New Password"
+										name="confirm_new_password"
+										autoComplete="confirm_new_password"
+										autoFocus
+										/>
+									</Grid>
+								</Grid>	
+									
+								<Grid item xs={12} sm={1}></Grid>
+								<Grid item xs={12} sm={3}>
+									<Grid item xs={12} sm={12} className={classes.uploadContainer}>
+										Upload your selfie 
+										<div className={classes.divtest}>
+											<img src="https://image.flaticon.com/icons/png/512/149/149092.png" width = "170px" height = "150px"></img>
+											<Grid item xs={12} sm={2}>
+												<div className={classes.picture}>
+													<Button 
+													type="submit"
+													fullWidth
+													variant="contained"
+													className={classes.buttonUpload}
+													href="#"
+													>
+													UPLOAD
+													</Button>
+													<Button 
+													type="submit"
+													fullWidth
+													variant="contained"
+													className={classes.buttonUpload}
+													href="#"
+													>
+													DELETE
+													</Button>
+												</div>
+											</Grid>    
+										</div>
+									</Grid>
+								</Grid>
+							
 							</Grid>
-						
-						</Grid>
-					</form>
-					{/* Next Button */}
-					<Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            className={classes.button}
-                            href="#"
-                          >
-                            UPDATE
-                          </Button>
-				</div>
+						</form>
+						{/* Next Button */}
+						<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								className={classes.button}
+								href="#"
+							  >
+								UPDATE
+							  </Button>
+					</div>
+				</Grid>
 			</Grid>
-		</Grid>
-	)
+		)
+	}else{
+		history.push('/')
+    	return(<h2>Home</h2>)
+	}
 }
 export default MyAccount
